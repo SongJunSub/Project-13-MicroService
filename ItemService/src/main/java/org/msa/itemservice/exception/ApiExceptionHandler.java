10fae6286017152e2bb113e4716ee9e509c9f5a0
Song Jun-Sub <song911130@gmail.com>
2024-05-16 22:07:26 +0900

    Add Exception Handler

---
+++ ItemService/src/main/java/org/msa/itemservice/exception/ApiExceptionHandler.java
@@ -0,0 +1,45 @@
+package org.msa.itemservice.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.msa.itemservice.dto.ResponseDTO;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> Exception(HttpServletRequest request, Exception e) throws Exception {
+        ResponseDTO.ResponseDTOBuilder responseDTOBuilder = ResponseDTO.builder();
+
+        responseDTOBuilder.code("500").message(e.getMessage());
+
+        return ResponseEntity.ok(responseDTOBuilder.build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException e) throws JSONException {
+        BindingResult bindingResult = e.getBindingResult();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(FieldError fieldError : bindingResult.getFieldErrors()) {
+            stringBuilder.append(fieldError.getField());
+            stringBuilder.append(" : ");
+            stringBuilder.append(fieldError.getDefaultMessage());
+            stringBuilder.append("/ 입력된 값 : ");
+            stringBuilder.append(fieldError.getRejectedValue());
+        }
+
+        ResponseDTO.ResponseDTOBuilder responseDTOBuilder = ResponseDTO.builder();
+
+        responseDTOBuilder.code("500").message(stringBuilder.toString());
+
+        return ResponseEntity.ok(responseDTOBuilder.build());
+    }
+
+}
