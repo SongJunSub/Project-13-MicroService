c94d946599a309ae747b53b8ba8ec9d45947944a
Song Jun-Sub <song911130@gmail.com>
2024-05-24 00:26:32 +0900

    JWT 인증을 위한 AuthenticationServer 구축

---
+++ AutenticationServer/src/main/java/org/msa/autenticationserver/AutenticationServerApplication.java
@@ -0,0 +1,13 @@
+package org.msa.autenticationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticationServerApplication.class, args);
+	}
+
+}
