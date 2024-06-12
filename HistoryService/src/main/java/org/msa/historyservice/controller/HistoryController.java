cc0dcd77b7268e4773988dc4acb31a151f4a3b02
Song Jun-Sub <song911130@gmail.com>
2024-06-12 18:13:30 +0900

    OpenFeign 의존성 추가

--- HistoryService/src/main/java/org/msa/historyservice/controller/HistoryController.java
+++ HistoryService/src/main/java/org/msa/historyservice/controller/HistoryController.java
@@ -1,6 +1,12 @@
 package org.msa.historyservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.msa.historyservice.dto.HistoryDTO;
+import org.msa.historyservice.dto.ResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "v1/history")
 public class HistoryController {
 
+    @GetMapping
+    public ResponseEntity<ResponseDTO> history(HttpServletRequest request, @RequestBody HistoryDTO historyDTO) throws Exception {
+        ResponseDTO.ResponseDTOBuilder responseBuilder = ResponseDTO.builder();
 
+        log.info("History : {}", historyDTO.toString());
+
+        responseBuilder.code("200").message("success");
+
+        return ResponseEntity.ok(responseBuilder.build());
+    }
 
 }
