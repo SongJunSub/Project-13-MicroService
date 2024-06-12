ff58644b53dcad534850746ecfdfb13f51af869c
Song Jun-Sub <song911130@gmail.com>
2024-06-12 17:59:59 +0900

    History Service Controller, DTO 생성

---
+++ HistoryService/src/main/java/org/msa/historyservice/controller/HistoryController.java
@@ -0,0 +1,14 @@
+package org.msa.historyservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "v1/history")
+public class HistoryController {
+
+
+
+}
---
+++ HistoryService/src/main/java/org/msa/historyservice/dto/ResponseDTO.java
@@ -0,0 +1,14 @@
+package org.msa.historyservice.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ResponseDTO {
+
+    private String code;
+
+    private String message;
+
+}
