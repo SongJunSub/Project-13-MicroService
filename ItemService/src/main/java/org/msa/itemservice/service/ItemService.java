b2a7ac680a5d708b34eda35c0ea534d658d1e3e6
Song Jun-Sub <song911130@gmail.com>
2024-06-13 17:10:17 +0900

    ActiveMQ를 이용한 History 전송 로직 구현

--- ItemService/src/main/java/org/msa/itemservice/service/ItemService.java
+++ ItemService/src/main/java/org/msa/itemservice/service/ItemService.java
@@ -1,11 +1,16 @@
 package org.msa.itemservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.jms.Queue;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.msa.itemservice.domain.Item;
 import org.msa.itemservice.dto.ItemDTO;
 import org.msa.itemservice.feign.HistoryFeignClient;
 import org.msa.itemservice.repository.ItemRepository;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +26,10 @@ public class ItemService {
     private final ItemRepository itemRepository;
     private final HistoryFeignClient historyFeignClient;
     private final RestTemplate restTemplate;
+    private final JmsTemplate jmsTemplate;
+    private final Queue activeMQ;
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     public void insertItem(ItemDTO itemDTO, String accountId) {
         SimpleDateFormat form = new SimpleDateFormat("yyyyMMddHHmmss");
@@ -45,7 +54,13 @@ public class ItemService {
         historyMap.put("itemId", itemDTO.getId());
 
         // log.info("Feign Result : {}", historyFeignClient.saveHistory(historyMap));
-        log.info("Rest Template Result : {}", restTemplate.postForObject("https://HISTORY-SERVICE/v1/history/save", historyMap, String.class));
+        // log.info("Rest Template Result : {}", restTemplate.postForObject("https://HISTORY-SERVICE/v1/history/save", historyMap, String.class));
+        try {
+            jmsTemplate.convertAndSend(activeMQ, objectMapper.writeValueAsString(itemDTO));
+        }
+        catch (JmsException | JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
